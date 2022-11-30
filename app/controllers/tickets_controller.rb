f13c2619bd6f674cc7d24717284057826e8b875c
Carlos Silveira <carlos.dt.cd@gmail.com>
2022-11-30 21:39:47 +0000

    add ticket action to controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,4 +3,30 @@ class TicketsController < ApplicationController
     @project = Project.find(params[:project_id])
     @tickets = Ticket.where("project_id = ?", @project.id)
   end
+
+  def new
+    @project = Project.find(params[:project_id])
+    @ticket = Ticket.new
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @ticket = Ticket.new(ticket_params)
+
+    if @ticket.save
+      redirect_to project_ticket_path(@project.id, @ticket.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @ticket = Ticket.find(params[:id])
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :description, :user_id, :status, :project_id)
+  end
 end

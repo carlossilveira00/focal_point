b24f2340c9a244266771496f8d7c49ddc65b8f86
Carlos Silveira <carlos.dt.cd@gmail.com>
2022-12-04 16:33:18 +0000

    Add connection between users and projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,20 @@
 class ProjectsController < ApplicationController
   def show
+    # @users will be an array of all the users related to this project.
+    @users = []
     @project = Project.find(params[:id])
     @tasks = Task.where("project_id = ?", @project.id)
+    # Get all the user_projects who have an invitation with the status accepted and an project_id of this project.
+    @users_assigned_to_project = UserProject.where(["project_id = ? and invitation_accepted = ?", @project.id, true])
+    # Loop and find the users through the user_id and push them into the @users array
+    @users_assigned_to_project.each do |user_project|
+      @users << User.find(user_project.user_id)
+    end
+    # Find the user who created the project and push them to the array aswell. Because
+    # you want to be able to select them to a task.
+    @users << @project_owner = User.find(@project.user_id)
     @new_task = Task.new
+    @new_user_project = UserProject.new
   end
 
   def new

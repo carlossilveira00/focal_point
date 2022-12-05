a0ebf8ed9a95ffb570e36cc917ce1d29551c0bfd
Carlos Silveira <carlos.dt.cd@gmail.com>
2022-12-05 15:32:49 +0000

    edit tasks

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -34,12 +34,19 @@ class ProjectsController < ApplicationController
   end
 
   def edit
+    @project = Project.find(params[:id])
   end
 
   def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    redirect_to project_path(@project)
   end
 
   def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to profile_path
   end
 
   private
